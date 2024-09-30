0850641a3fa5850722f5e453b530a5c358f782cd
Karthik <karthik.s2022cce@sece.ac.in>
2024-09-30 19:18:13 +0530

    comments

--- Sorting/bubbleSort.cpp
+++ Sorting/bubbleSort.cpp
@@ -4,12 +4,12 @@ using namespace std;
 void bubbleSort ( vector<int>& arr ) {
     for ( int i = 0; i < arr.size(); i++ ) {
         bool flag = false;
-        for ( int j = 0; j < arr.size()-i; j++ ) {
+        for ( int j = 0; j < arr.size()-i; j++ ) { // we keep on swapping larger values and finally push it back of array
             if ( arr[j] > arr[j+1] ) {
                 flag = false;
                 swap ( arr[j], arr[j+1] );
             }
-            if ( flag )
+            if ( flag ) // if no swap, it means already sorted
                 break;
         }
     }
